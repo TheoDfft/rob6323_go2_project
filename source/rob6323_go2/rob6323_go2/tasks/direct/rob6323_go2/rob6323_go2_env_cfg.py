83d982644b1ccc1f97664ffcb637bcc31193b15b
TheoDfft <duffaut.theo@gmail.com>
2025-12-19 01:20:40 -0500

    RT: lowered lin vel tracking because the robots were jumping in place

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -146,7 +146,7 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     current_vel_visualizer_cfg.markers["arrow"].scale = (0.5, 0.5, 0.5)
 
     # reward scales
-    lin_vel_reward_scale = 10.0
+    lin_vel_reward_scale = 1.0
     yaw_rate_reward_scale = 0.1
     # === ADDED: Action rate penalty ===
     action_rate_reward_scale = -0.1
